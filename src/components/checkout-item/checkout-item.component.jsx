e018fd8fe2151ebdf4bcc4c0594e4f82a2650157
AdiDD <adi.deaconu21@gmail.com>
2020-04-14 23:53:49 +0300

    Added addItem and removeItem functionality to checkout item  component by pressing + and - icons

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import { clearItemFromCart } from "../../redux/cart/cart.actions";
+import { ReactComponent as PlusIcon } from "../../assets/plus-icon.svg";
+import { ReactComponent as MinusIcon } from "../../assets/minus-icon.svg";
+
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from "../../redux/cart/cart.actions";
 
 import "./checkout-item.styles.scss";
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className="checkout-item">
@@ -13,7 +20,15 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
         <img src={imageUrl} alt={`${name} item`} />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="icon minus" onClick={() => removeItem(cartItem)}>
+          <MinusIcon />
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="icon plus" onClick={() => addItem(cartItem)}>
+          <PlusIcon />
+        </div>
+      </span>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={() => clearItem(cartItem)}>
         &#10005;
@@ -24,6 +39,8 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
 
 const mapDispatchToProps = (dispatch) => ({
   clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
 });
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
